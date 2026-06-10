4f3e65eb4ea30bc0970a64bf200224d0e8b29933
Joao_VM <jmalustosa@gmail.com>
2026-06-10 17:35:58 -0300

    feat: add and test 003-bad-upstream incident

---
+++ incidents/003-bad-upstream/incident.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo sed -i \
+'s|proxy_pass http://127.0.0.1:8000;|proxy_pass http://127.0.0.1:9000;|' \
+/etc/nginx/sites-available/default
+
+if ! sudo systemctl restart nginx; then
+    echo "ERROR: Incident injection failed."
+    exit 1
+fi
